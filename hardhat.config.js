0da3e9e259d331ad0da6bb1bd1b2ccff3642ebd0
Ankush Kumar <imankush1010@gmail.com>
2024-07-26 23:14:48 +0530

    Final version NFT bridge

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { POLYGON_API_URL, SEPOLIA_API_URL, PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    polygon_amoy: {
+      url: POLYGON_API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    sepolia: {
+      url: SEPOLIA_API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
