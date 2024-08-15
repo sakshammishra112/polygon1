1e1dfd253e06cb3cfc9649a61b23b65da22256a3
sakshammishra112 <158152958+sakshammishra112@users.noreply.github.com>
2024-08-15 15:49:40 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -14,19 +14,19 @@ const walletAddress = "0x3FB380D3E981106F962486e91D7E558E2815E953"; // place you
 
 async function main() {
   const imageUrls = [
-    "https://bafybeihfa4kxlbq7rq364qi76kq3xpg4ot2fpzk6qxpgqc7wboji2atgry.ipfs.dweb.link/",
-    "https://bafybeiep5f5nwruxsmbi4pgiizdm7t4u222uy26yg7o4ds2zm5syptwdsy.ipfs.dweb.link/",
-    "https://bafybeibyr5tc4qkvftb47cpiicfwyxnerxlujhdrcuf4orkzd7gblw3ooe.ipfs.dweb.link/",
-    "https://bafybeif272oslaqvdjma4c7nk3p4nnq2gsjkdcoufgpkp74skl7g3w4c24.ipfs.dweb.link/",
-    "https://bafybeibffxqcgdel6ca5p637q5pkfdwv7boodr64bc57dp6cgr77g5ihy4.ipfs.dweb.link/",
-  ];
+    "https://cyan-manual-crayfish-417.mypinata.cloud/ipfs/QmQ9DDwVLY3faExFLSFcHBwK7sWoEqsdtprzuPVwH9H9HM",
+    "https://cyan-manual-crayfish-417.mypinata.cloud/ipfs/Qma1sbZjjLek5ZqPYG3owN4EDJ1h1BQo7pgMQMjHLN7iwF",
+    "https://cyan-manual-crayfish-417.mypinata.cloud/ipfs/QmWf2WbDqmUbxZgtqKmxLhay2oihJFFYsLaMLXCnmjKHq3",
+    "https://cyan-manual-crayfish-417.mypinata.cloud/ipfs/QmQzwmBUqBwjE3sHEUq4xJFzppbFGyUjXFqa1UpWZMuF3W",
+    "https://cyan-manual-crayfish-417.mypinata.cloud/ipfs/QmZiyVmHckbzR5bgFW6H9PH5U4PTUUFabLEVortDGuihw9",
+  ];
 
   const prompts = [
-    "imagine an image of super snake with 7 heads. Make it a deity with super powers",
-    "imagine an image of an eagle soaring through the sky, with magical super power aura around it",
-    "imagine an image of a ninja in shadowy background with a shiny and reflecting blade",
-    "imagine an image of earth. Make the earth wear goggles driving a kart",
-    "imagine an image of a cat as a superman with glowing eyes and super energy beam with super power aura",
+    "A futuristic city skyline at sunset",
+    "A serene mountain landscape with a river",
+    "An abstract painting of a colorful explosion",
+    "A fantasy world with dragons and castles",
+    "A detailed map of an imaginary land"
   ];
 
   const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
